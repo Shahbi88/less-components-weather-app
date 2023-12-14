5cf985106c8d47641858ec331fa119e17dc39585
Shahbi88 <rihabazar@gmail.com>
2023-12-14 15:54:51 +0000

    correcting the Github link

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,10 @@ import "./styles.css";
 export default function Footer() {
   return (
     <h6 className="footer">
-      <a href="https://github.com/Shahbi88/Forecast" target="_blank">
+      <a
+        href="https://github.com/Shahbi88/less-components-weather-app/tree/master/src"
+        target="_blank"
+      >
         {" "}
         Open-source code{" "}
       </a>
